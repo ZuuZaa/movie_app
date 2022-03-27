cbc7320b2e0b4d1fbc1ed1a8e5c8e9027913da9e
Zumrud Nerman <48273585+ZuuZaa@users.noreply.github.com>
2022-03-28 00:41:26 +0400

    api fetch is ready

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,24 @@
-import { MovieListing } from "components/MovieListing"
-import { useEffect } from "react"
+import { MovieListing } from "components/MovieListing";
+import { useEffect } from "react";
+import MovieApi from 'common/api/movieAPI';
+import { ApiKey } from "common/api/movieApiKey";
 
 export const Home = () => {
 
 useEffect(()=>{
-    const fetchMovies = ""
+    const movieText = "Harry";
+    const fetchMovies = async ()=>{
+        const response = await MovieApi
+        .get(`?apikey=${ApiKey}&s=${movieText}&type=movie`)
+        .catch(err => console.log("error", err));
+        console.log(response);
+    }
+    fetchMovies();
 },[])
 
     return (
         <div>
-            <div className="banner-image"></div>
+            <div className="banner-img"></div>
             <MovieListing />
         </div>
     )
